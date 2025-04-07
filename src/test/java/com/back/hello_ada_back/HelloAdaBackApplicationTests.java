c022cd36fedc2c602ab574c34093ad1f170fac8c
CharlotteBCK <charlotte44300@gmail.com>
2025-04-07 16:05:06 +0200

    installation springboot

---
+++ src/test/java/com/back/hello_ada_back/HelloAdaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.hello_ada_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloAdaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
